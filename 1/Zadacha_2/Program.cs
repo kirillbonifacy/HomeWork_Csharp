21b6dc3dd9628ed06944cdc2463d90cd8d163f7a
kirill <kirill@gmail.com>
2022-11-16 01:45:00 +0300

    Zadacha 2

---
+++ 1/Zadacha_2/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+Console.WriteLine("Введите число a: ");
+int a = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine("Введите число b: ");
+int b = int.Parse(Console.ReadLine()!);
+
+if(a > b)
+{
+    Console.WriteLine($"Болше {a}");
+}
+else
+{
+    Console.WriteLine($"Болше {b}");
+}
