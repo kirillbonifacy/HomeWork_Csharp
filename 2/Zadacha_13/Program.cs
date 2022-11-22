234231617e9745cc5dd41f4e8d527f4a7dd313d1
kirill <kirill@gmail.com>
2022-11-22 19:35:19 +0300

    Zadacha 13

---
+++ 2/Zadacha_13/Program.cs
@@ -0,0 +1,35 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет. Берем числа до 100000
+
+
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int MakeArray(int a, int b)
+{
+int result = -1;
+    if (b < 3)
+    {
+        Console.Write("Третьей цифры нет ");
+    }
+    else
+    {
+        int c = 1;
+        for (int i = b; i > 3; i--)
+        {
+            c = c * 10;
+        }
+
+        result = (a / c) % 10;
+    }
+return result;
+}
+
+int n = ReadInt("Введите число: ");
+int count = n.ToString().Length;
+Console.Write(MakeArray(n, count));
+
+
+
